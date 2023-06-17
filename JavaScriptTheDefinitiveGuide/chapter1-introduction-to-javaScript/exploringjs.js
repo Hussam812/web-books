7f4b535ba4554a1aca301424b4ac4ec6968c0d8d
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-17 19:09:02 +0200

    1.1-exploring javaScript

---
+++ JavaScriptTheDefinitiveGuide/chapter1-introduction-to-javaScript/exploringjs.js
@@ -0,0 +1,13 @@
+// 1.1 exploring javaScript
+let primes =[2, 3, 4, , 7];
+console.log(primes[primes.length-1]);
+console.log(primes[0] + primes[1]);
+
+function fact(x){
+    if(x > 1) return x * fact(x-1);
+    else return 1;
+}
+
+console.log(fact(4));
+console.log(fact(5));
+console.log(fact(6));
